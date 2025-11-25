3b942ff804015c7731e3daeca9f4ea4949325752
Karyna Pomin <115844791+KarynaPmn@users.noreply.github.com>
2025-11-25 09:22:19 +0100

    Implement mathematical functions in Zadania_1.kt
    Added several mathematical functions including Fibonacci, factorial, and sum of digits.

--- Kotlin/Zadania_1.kt
+++ Kotlin/Zadania_1.kt
@@ -81,3 +81,75 @@ open class Koszyk(private val id : Int, var opis : String, var kwota : Float);
 class Faktura(val id : Int, var wartosc : Float)
 class Kurier(val id : Int, var nazwa : String)
 class Zamowienie(var koszyk: Koszyk, var faktura: Faktura, var kurier: Kurier)
+
+fun d_1() : Int {
+    var s = 0
+    for (i in 10 until 100) s += i
+
+    return s
+}
+
+fun d_2(n: Int) : Int {
+    var x = n
+    var s = 0
+    while (x > 0) {
+        s += x % 10
+        x /= 10
+    }
+
+    return s
+}
+
+fun d_3(n : Int) : IntArray {
+    var T = IntArray(0)
+    var i = 1
+    do {
+        if (n % i == 0)
+            T += i
+        i++
+    } while (i <= n)
+
+    return T
+}
+
+fun d_3v_2(n : Int) : MutableList<Int> {
+    var T = mutableListOf(0)
+    var i = 1
+    do {
+        if (n % i == 0)
+            T.add(i)
+        i++
+    } while (i <= n)
+
+    return T // .toList()
+}
+
+fun Fibo(n : Int) : Int {
+    if (n == 1 || n == 2) return 1
+    return Fibo(n - 1) + Fibo(n - 2)
+}
+
+fun FiboList(n : Int) : MutableList<Int> {
+    val L = mutableListOf(0)
+    for(i in 1 .. n) L.add(Fibo(i))
+
+    return L
+}
+
+fun Silnia(n : Int) : Int {
+    if (n == 0 || n == 1) return 1
+    return n * Silnia(n - 1)
+}
+
+fun d_5(t : String) : Map<Int, Int> {
+    val W = t.split(" ")
+    val N = W.map { it.toInt() }
+    val M = mutableMapOf<Int, Int>()
+
+    for (n in N) {
+//        M[w.toInt()] = Silnia(w.toInt())
+        M[n] = Silnia(n)
+    }
+
+    return M
+}
